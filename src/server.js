dcf9c9cdd7b84c447e266e6d6f296a9f1f28a61b
aaditya0204 <adityarajcareer123@gmail.com>
2026-02-05 04:46:07 +0530

    Done with Database connection set-up and server configuration.

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express from "express";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config({
+  path: "../.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Has Started at PORT:${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERRR", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log(" DataBase Connection Failed!! ", err);
+  }); 
